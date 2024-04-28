3996f2a37e2340769a711768228fb3a3242dc81d
daniilkuz <d.d.kuzovlev@gmail.com>
2024-04-29 04:17:48 +0700

    added logic to getMovie function

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 func deleteMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item = range movies {
+	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
 			break
@@ -40,7 +40,14 @@ func deleteMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Typpe", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+	}
 }
 
 func main() {
